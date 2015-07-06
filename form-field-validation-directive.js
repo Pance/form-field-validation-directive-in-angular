c1b1d3b929e4a2a54eb44d259561d0d028975283
Pance <pbarnett888@gmail.com>
2015-07-06 14:29:16 -0500

    Rename 'field-validation' directive to 'form-field-validation'

--- form-field-validation-directive.js
+++ form-field-validation-directive.js
@@ -3,7 +3,7 @@ angular.module('someApp')
     return {
       restrict: 'E',
       transclude: 'true',
-      templateUrl: 'field-validation.html',
+      templateUrl: 'form-field-validation.html',
       link: {
         pre: function(scope, element, attrs) {
           scope.formField = attrs.formField;
