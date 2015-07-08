723549da8720ca61fc97ecb714ebb257d009ccc6
Pance <pbarnett888@gmail.com>
2015-07-08 10:50:50 -0500

    Add the validationErrorMessages directive

--- form-field-validation-directive.js
+++ form-field-validation-directive.js
@@ -10,4 +10,41 @@ angular.module('someApp')
         }
       }
     };
+  })
+  .directive('validationErrorMessages', function() {
+    // This directive will not work unless it is a child of a formFieldValidation directive
+
+    return {
+      restrict: 'E',
+      transclude: 'true',
+      templateUrl: 'validation-error-messages.html',
+      scope: false, // use the same scope as the parent
+      link: function(scope, element, attrs) {
+        var formFieldSplit = scope.$parent.formField.split('.');
+        var formName = formFieldSplit[0];
+        var fieldName = formFieldSplit[1];
+
+        //watch the form input field object for a couple of flags
+        scope.$watchGroup([
+            "$parent."+formName+"."+fieldName+".$invalid",
+            "$parent."+formName+"."+fieldName+".$dirty"],
+          determineVisibility);
+
+        function determineVisibility(newValue) {
+          var dvFormField = scope.$parent[formName][fieldName];
+          // The links for this directive will be set up when the field does not exist in the form yet
+          if(_.isUndefined(dvFormField)){
+            // don't try to do anything with the form field because it is undefined, at the moment
+            return;
+          }
+
+          if(dvFormField.$invalid && dvFormField.$dirty) {
+            // we have some errors to show
+            element.removeAttr('hidden');
+          } else {
+            element.attr('hidden', '');
+          }
+        }
+      }
+    };
   });
